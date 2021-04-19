8eea52c4476af484d8afbf6a1a607bad6a0efe59
gewlar <luca.schafroth@gmail.com>
2021-04-19 18:41:51 +0200

    Include theme loader

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,7 @@ module.exports = function (config) {
     })
 
     config.addPassthroughCopy('src/assets/images')
+    config.addPassthroughCopy('src/assets/themes')
     config.addPassthroughCopy('src/prev')
     config.addPassthroughCopy('src/next')
     config.addPassthroughCopy('src/data/members.json')

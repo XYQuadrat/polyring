ce7f98ea0384d3294f511dea33eeb9186f2eb163
xyquadrat <mail@xyquadrat.ch>
2022-11-26 14:26:35 +0100

    Use minified SVG, update to new dart-sass API

--- src/assets/styles/styles.11ty.js
+++ src/assets/styles/styles.11ty.js
@@ -19,36 +19,19 @@ module.exports = class {
         }
     }
 
-    // Compile Sass to CSS,
-    // Embed Source Map in Development
-    async compile(config) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                config.sourceMap = true
-                config.sourceMapEmbed = true
-                config.outputStyle = 'expanded'
-            }
-            return sass.render(config, (err, result) => {
-                if (err) {
-                    return reject(err)
-                }
-                resolve(result.css.toString())
-            })
-        })
+    // Compile Sass to CSS
+    compile(scssFile) {
+        return sass.compile(scssFile, {
+            style : isProd ? 'compressed' : 'expanded'
+        }).css;
     }
 
     // Minify & Optimize with CleanCSS in Production
-    async minify(css) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                resolve(css)
-            }
-            const minified = new CleanCSS().minify(css)
-            if (!minified.styles) {
-                return reject(minified.error)
-            }
-            resolve(minified.styles)
-        })
+    minify(css) {
+        if (!isProd) {
+            return css
+        }
+        return new CleanCSS().minify(css).styles
     }
 
     // display an error overlay when CSS build fails.
@@ -99,16 +82,12 @@ module.exports = class {
     // render the CSS file
     async render({ entryPath }) {
         try {
-            const css = await this.compile({ file: entryPath })
-            const result = await this.minify(css)
-            return result
+            const css = this.compile(entryPath)
+            return this.minify(css)
         } catch (err) {
-            // if things go wrong
             if (isProd) {
-                // throw and abort in production
                 throw new Error(err)
             } else {
-                // otherwise display the error overly
                 console.error(err)
                 const msg = err.formatted || err.message
                 return this.renderError(msg)
